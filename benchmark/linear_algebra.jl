8d254b387b6fd398b00171c23e87a3f5ab6ab97e
Yongjun Xu <yongjunx23@gmail.com>
2026-08-12 22:12:40 +0800

    Benchmark explicit MultiFloatVec GEMM microkernels

--- benchmark/linear_algebra.jl
+++ benchmark/linear_algebra.jl
@@ -21,19 +21,29 @@ function bench_gemm(::Type{M}, n) where {M<:MultiFloat}
     A = rand(M, n, n)
     B = rand(M, n, n)
     Cfast = zeros(M, n, n)
+    Cvec4 = similar(Cfast)
+    Cvec8 = similar(Cfast)
     Cgeneric = similar(Cfast)
 
     fast!() = MFLA.gemm!(Cfast, A, B)
+    vec4!() = MFLA._gemm_vec!(Cvec4, A, B, Val(4))
+    vec8!() = MFLA._gemm_vec!(Cvec8, A, B, Val(8))
     generic!() = mul!(Cgeneric, A, B)
 
-    fast!(); generic!()
+    fast!(); vec4!(); vec8!(); generic!()
     @assert all(MultiFloats.isnormalized, Cfast)
+    @assert all(MultiFloats.isnormalized, Cvec4)
+    @assert all(MultiFloats.isnormalized, Cvec8)
+    @assert all(i -> Cvec4[i] === Cfast[i], eachindex(Cfast))
+    @assert all(i -> Cvec8[i] === Cfast[i], eachindex(Cfast))
 
     tf = minimum_time(fast!)
+    t4 = minimum_time(vec4!)
+    t8 = minimum_time(vec8!)
     tg = minimum_time(generic!)
     work = 2n^3
-    @printf("  GEMM n=%d: direct-FMA=%8.3f ms (%7.1f MFLOP/s), generic=%8.3f ms, speedup=%5.2fx\n",
-            n, 1e3tf, work/tf/1e6, 1e3tg, tg/tf)
+    @printf("  GEMM n=%d: streaming=%8.3f ms (%7.1f MFLOP/s), Vec4=%8.3f ms (%5.2fx), Vec8=%8.3f ms (%5.2fx), generic=%8.3f ms, streaming/generic=%5.2fx\n",
+            n, 1e3tf, work/tf/1e6, 1e3t4, tf/t4, 1e3t8, tf/t8, 1e3tg, tg/tf)
 end
 
 function bench_gemv(::Type{M}, m, n) where {M<:MultiFloat}
@@ -76,7 +86,7 @@ for M in (Float64x2, Float64x4)
     println(M)
     bench_dot(M, 20_000)
     bench_gemv(M, 256, 128)
-    for n in (16, 32, 48)
+    for n in (16, 32, 48, 64)
         bench_gemm(M, n)
     end
 end
